6b39c256102b5c3106a755efcad77f12e83d3cad
ayan <ayanrasulova13@gmail.com>
2026-03-22 00:57:15 -0400

    updated servo

--- Ayan_Servo_Demo.ino
+++ Ayan_Servo_Demo.ino
@@ -1,58 +1,73 @@
 #include <Servo.h>
 
-Servo myservo;
+Servo wrist;
+Servo thumb;
+Servo pointer;
+Servo middle;
+Servo ring;
+Servo pinky;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  myservo.attach(5);
+
+  wrist.attach(5);
+  thumb.attach(6);
+  pointer.attach(9);
+  middle.attach(10);
+  ring.attach(11);
+  pinky.attach(3);
+
 
 }
 
-char check_checksum (char* buf){
-    char check = buf[0];
-    for (int i = 1; i < 6; ++i){
+byte check_checksum(byte* buf) {
+    byte check = buf[0];
+    for (int i = 1; i < 6; ++i) {
         check ^= buf[i];
     }
 
-    if (check == buf[6]){
+    if (check == buf[6]) {
         return 1;
     }
+    return 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  char val = 0;
-  char checked = 0;
-  char* vals;
+  byte val = 0;
+  byte checked = 0;
+  byte vals[7]; 
 
-  while (Serial.available() != 0){
+  while (Serial.available() > 0) {
     val = Serial.read();
-    if (val == 190){ // start byte found
-        Serial.readBytes(vals, 7); 
+    if (val == 190) {  // start byte
+        Serial.readBytes(vals, 7);
+
         checked = check_checksum(vals);
-        if (checked){
-            for (int i = 0; i<6; ++i){
+        if (checked) {
+            Serial.println("Valid packet:");
+
+            wrist.write(vals[0]);
+            thumb.write(vals[1]);
+            pointer.write(vals[2]);
+            middle.write(vals[3]);
+            ring.write(vals[4]);
+            pinky.write(vals[5]);
+
+            for (int i = 0; i < 6; ++i) {
                 Serial.println(vals[i]);
-                myservo.write(vals[0]);
+            }
+
+        } else {
+            Serial.println("Checksum failed!!! Data collected:");
+            for (int i = 0; i < 7; ++i) {
+                Serial.println(vals[i]);
             }
         }
-        else {
-          Serial.println("Checksum failed!!! Data collected: ");
-           for (int i = 0; i<7; ++i){
-                Serial.println(vals[i]);
-            }
-        }
-
-
-    }
+    } 
     else {
-      Serial.print("Start byte not found! Insted found ");
-      Serial.println(val);
+        Serial.print("Start byte not found! Instead found: ");
+        Serial.println(val);
     }
-
   }
-
-
 }
-
